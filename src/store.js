c913e94c3b82b1ec1d1ce3ed259f2ca1c1cbaa4d
Lauren Morrow <morrowlauren17@gmail.com>
2019-02-09 16:49:24 -0800

    changed store to deploy

--- src/store.js
+++ src/store.js
@@ -1,11 +1,6 @@
 import {createStore} from 'redux'
 import {reducer as formReducer} from 'redux-form';
 
-import {trelloReducer} from './reducers';
+// import {trelloReducer} from './reducers';
 
-export default createStore(
-	combineReducers({
-		form: formReducer,
-		trello: trelloReducer
-	})
-);
+export default createStore();
