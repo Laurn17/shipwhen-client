ea27b6d2697d1970cc957389d663f2913000e2c7
Lauren Morrow <morrowlauren17@gmail.com>
2019-05-08 22:12:26 -0700

    pullin in list of reviews to bus-page

--- src/components/bus-page.js
+++ src/components/bus-page.js
@@ -12,45 +12,73 @@ class BusPage extends React.Component {
  //        this.props.dispatch(submitReview(title));
  //    }
 
-	render () {
-console.log(this.props.arrive);
+// 	render () {
 		 
-		return (
-			<div className="reviews1">
-			console.log(this.props.bus_name)
-			<h1>Reviews For {this.props.bus_name}</h1>
-				<div className="indivReview">
-					<div className="revDetails">
-						<p>{this.props.date_created}</p>
-						<p>{this.props.created_by}</p>
-					</div>
-					<p>{this.props.bus_name}</p>
-					<p>Delivery Type: {this.props.delivery}</p>
-					<p>Ordered On: {this.props.order_date}</p>
-					<p>Estimated to Arrive: {this.props.estimate_date}</p>
-					<p>Package Arrived: {this.props.arrive}</p>
-					<p>Arrival Date: {this.props.arrive_date}</p>
-				</div>
+// 		return (
+// 			<div className="reviews1">
+// 			<h1>Reviews For {this.props.bus_name}</h1>
+// 				<div className="indivReview">
+// 					<div className="revDetails">
+// 						<p>{this.props.date_created}</p>
+// 						<p>{this.props.created_by}</p>
+// 					</div>
+// 					<p>{this.props.bus_name}</p>
+// 					<p>Delivery Type: {this.props.delivery}</p>
+// 					<p>Ordered On: {this.props.order_date}</p>
+// 					<p>Estimated to Arrive: {this.props.estimate_date}</p>
+// 					<p>Package Arrived: {this.props.arrive}</p>
+// 					<p>Arrival Date: {this.props.arrive_date}</p>
+// 				</div>
 
-				<section id="review-page">
-					< ReviewFormPage />
-				</section>
-			</div>
-		);
-	}
-}
+// 				<section id="review-page">
+// 					< ReviewFormPage />
+// 				</section>
+// 			</div>
+// 		);
+// 	}
+// }
+
 // busReviews is the name of the reducer in my Store
-const mapStateToProps = state => {
-	return {
-		date_created: state.busReviews.reviews[0].date_created,
-		bus_name: state.busReviews.reviews[0].bus_name,
-		delivery: state.busReviews.reviews[0].delivery,
-		order_date: state.busReviews.reviews[0].order_date,
-		estimate_date: state.busReviews.reviews[0].estimate_date,
-		arrive: state.busReviews.reviews[0].arrive.toString(),
-		arrive_date: state.busReviews.reviews[0].arrive_date,
-		created_by: state.busReviews.reviews[0].created_by
-	}
-};
+// const mapStateToProps = state => {
+// 	return {
+// 		date_created: state.busReviews.reviews[0].date_created,
+// 		bus_name: state.busReviews.reviews[0].bus_name,
+// 		delivery: state.busReviews.reviews[0].delivery,
+// 		order_date: state.busReviews.reviews[0].order_date,
+// 		estimate_date: state.busReviews.reviews[0].estimate_date,
+// 		arrive: state.busReviews.reviews[0].arrive.toString(),
+// 		arrive_date: state.busReviews.reviews[0].arrive_date,
+// 		created_by: state.busReviews.reviews[0].created_by
+// 	}
+// };
+
+// --------------------TESTING PULLING IN MY ARRAY OF REVIEWS FROM REVIEW.JS-----------------
+	render() {
+	        const reviews = this.props.reviews.map((review, index) => (
+	            <li className="review-wrapper" key={index}>
+	                <Review index={index} {...review} />
+	            </li>
+	        ));
+
+	        return (
+	            <div className="busPage">
+	                <h1>Reviews For {this.props.busName}</h1>
+	                <div className="reviews1">
+	                    {reviews}
+	                </div>
+	                <section id="review-page">
+						< ReviewFormPage />
+					</section>
+	            </div>
+	        );
+	    };
+	};
+
+const mapStateToProps = state => ({
+    reviews: state.busReviews.reviews,
+    busName: state.busReviews.reviews[0].bus_name
+});
+
+// --------------------------------------TESTING SECTION END--------------------------------------
 
 export default connect(mapStateToProps)(BusPage);
--- src/components/review.js
+++ src/components/review.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-// import './review.css';
+import './review.css';
 
-export default function Review(props) {
-    return (
-        <div className="review">
-            {props.text}
-        </div>
-    );
+class Review extends React.Component {
+    
+	render() {
+    	return (
+			<div className="reviewPage">
+				<div className="indivReview">
+					<div className="revDetails">
+						<p>{this.props.date_created}</p>
+						<p>{this.props.created_by}</p>
+					</div>
+					<p>{this.props.bus_name}</p>
+					<p>Delivery Type: {this.props.delivery}</p>
+					<p>Ordered On: {this.props.order_date}</p>
+					<p>Estimated to Arrive: {this.props.estimate_date}</p>
+					<p>Package Arrived: {this.props.arrive}</p>
+					<p>Arrival Date: {this.props.arrive_date}</p>
+				</div>
+			</div>
+    	);
+    }
+}
+
+const mapStateToProps = state => {
+	return {
+		date_created: state.busReviews.reviews[0].date_created,
+		bus_name: state.busReviews.reviews[0].bus_name,
+		delivery: state.busReviews.reviews[0].delivery,
+		order_date: state.busReviews.reviews[0].order_date,
+		estimate_date: state.busReviews.reviews[0].estimate_date,
+		arrive: state.busReviews.reviews[0].arrive.toString(),
+		arrive_date: state.busReviews.reviews[0].arrive_date,
+		created_by: state.busReviews.reviews[0].created_by
+	}
 };
 
-// Card.defaultProps = {
-//     text: ''
-// };
+export default connect(mapStateToProps)(Review);
