b28b11018c473048c485c2a36974c12225ec9f14
Lauren Morrow <morrowlauren17@gmail.com>
2019-05-26 18:08:25 -0700

    completed media queries

---
+++ src/actions/bus-reviews.test.js
@@ -0,0 +1,39 @@
+import {
+    FETCH_BUS_REQUEST
+    fetchBusRequest
+    FETCH_BUS_SUCCESS
+    fetchBusSuccess
+    FETCH_BUS_ERROR
+    fetchBusError
+    FETCH_BUS_NODATA
+    fetchBusNoData
+    FETCH_REVIEW_ERROR
+    fetchReviewError
+    FETCH_REVIEW_SUCCESS
+    fetchReviewSuccess
+} from './bus-reviews';
+
+// describe('restartGame', () => {
+//     it('Should return the action', () => {
+//         const correctAnswer = 10;
+//         const action = restartGame(correctAnswer);
+//         expect(action.type).toEqual(RESTART_GAME);
+//         expect(action.correctAnswer).toEqual(correctAnswer);
+//     });
+// });
+
+// describe('makeGuess', () => {
+//     it('Should return the action', () => {
+//         const guess = 10;
+//         const action = makeGuess(guess);
+//         expect(action.type).toEqual(MAKE_GUESS);
+//         expect(action.guess).toEqual(guess);
+//     });
+// });
+
+// describe('generateAuralUpdate', () => {
+//     it('Should return the action', () => {
+//         const action = generateAuralUpdate();
+//         expect(action.type).toEqual(GENERATE_AURAL_UPDATE);
+//     });
+// });
--- src/components/signup-page.js
+++ src/components/signup-page.js
@@ -11,13 +11,15 @@ export function SignUpPage(props) {
 
 	return (
 	  <div className="signup-page">
-      <div className="col-6">
-        <h1>Find Actual Business Shipping Times.</h1>
-        <img alt="shipping truck" src="https://cdn2.iconfinder.com/data/icons/blue-transports-3/262/Untitled-2-512.png" />
-      </div>
-      <div className="col-6">
-        <h2>sign up to post helpful reviews</h2>
-        <SignupForm /> 
+      <div className="colContainer">
+        <div className="col-6 one">
+          <h1>Find Actual Business Shipping Times</h1>
+          <img alt="shipping truck" src="https://cdn2.iconfinder.com/data/icons/blue-transports-3/262/Untitled-2-512.png" />
+        </div>
+        <div className="col-6" id="colTwo">
+          <h2>sign up to post helpful reviews</h2>
+          <SignupForm /> 
+        </div>
       </div>
     </div>
 	);
