33d0dd2875bcde39e2f781dfa4bd8486fd7f089b
Lauren Morrow <morrowlauren17@gmail.com>
2019-05-27 21:41:41 -0700

    reducer test working

--- src/reducers/bus-reviews.test.js
+++ src/reducers/bus-reviews.test.js
@@ -1,26 +1,25 @@
-// import {busReviewsReducer} from './bus-reviews';
-// import * as actions from '../actions/bus-reviews';
+import busReviewsReducer from './bus-reviews';
+import * as actions from '../actions/bus-reviews';
 
-// describe('busReviewsReducer', () => {
-//     // Set up some dummy data
-//     const bus_name = 'Amazon';
+describe('busReviewsReducer', () => {
+    // Set up some dummy data
+    const sampleState = {
+        bus_name: null,
+        reviews: [],
+        error: null,
+        submitSucceeded: null,
+        noData: null,
+        loading: null
+    }
 
+    it('Should set the initial state when nothing is passed in', () => {
+        const state = busReviewsReducer(undefined, {type: '__UNKNOWN'});
+        expect(state).toEqual(Object.assign({}, sampleState));
+    });
 
-//     it('Should set the initial state when nothing is passed in', () => {
-//         const state = busReviewsReducer(undefined, {type: '__UNKNOWN'});
-//         expect(state).toEqual({
-//             bus_name: null,
-//             reviews: [],
-//             error: null,
-//             submitSucceeded: null,
-//             noData: null,
-//             loading: null
-//         });
-//     });
-
-//     it('Should return the current state on an unknown action', () => {
-//         let currentState = {};
-//         const state = busReviewsReducer(currentState, {type: '__UNKNOWN'});
-//         expect(state).toBe(currentState);
-//     });
-// });
+    it('Should return the current state on an unknown action', () => {
+        let currentState = {};
+        const state = busReviewsReducer(currentState, {type: '__UNKNOWN'});
+        expect(state).toEqual({});
+    });
+});
