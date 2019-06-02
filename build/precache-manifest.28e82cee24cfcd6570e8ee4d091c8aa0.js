9b60849c789c0d08e2f3a882fdabee72933d8262
Lauren Morrow <morrowlauren17@gmail.com>
2019-06-02 12:11:56 -0700

    re-do build

---
+++ build/precache-manifest.28e82cee24cfcd6570e8ee4d091c8aa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7606dacff630d8bf22be5411acc659c2",
+    "url": "/static/media/landing-background1.7606dacf.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd25a7468e460416c030",
+    "url": "/static/js/main.dd25a746.chunk.js"
+  },
+  {
+    "revision": "c73c4182ca6914e0c9ea",
+    "url": "/static/js/1.c73c4182.chunk.js"
+  },
+  {
+    "revision": "dd25a7468e460416c030",
+    "url": "/static/css/main.ff4cd00c.chunk.css"
+  },
+  {
+    "revision": "83333b5b138372760e0add546f0e8a0c",
+    "url": "/index.html"
+  }
+];
